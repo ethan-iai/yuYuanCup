ff26fb2d3d1bfca01079d7e007280697ce43994a
Liu <2276145687@qq.com>
2020-10-23 22:41:35 +0800

    外环PID算法已更新

--- PID/PID_external/PIDalgorithm.py
+++ PID/PID_external/PIDalgorithm.py
@@ -1,8 +1,7 @@
-import math
-
 class PID:
+
     def __init__(self):
-        self.last_delta = 0
+        self.last_delta_pixel = 0
         self.integrator = 0
         self.inte_max = 100
         self.Kp = 0.5 #比例增益,与比例度呈倒数关系
@@ -14,26 +13,57 @@ class PID:
         self.expected_speed_D = 0.0
         self.maxspeed = 10.0
 
+    def velocity_function(self, dicision_coefficient):
+        fun_name = "velocity_function_" + str(dicision_coefficient)
+        velocity_method = getattr(self, fun_name, self.velocity_function_other)
+        return velocity_method
+
+    def velocity_function_0(self, distance_coefficient, direction_coefficient):
+        self.expected_speed_A = self.expected_speed_B = self.expected_speed_C =self.expected_speed_D = self.maxspeed * distance_coefficient
+
+    def velocity_function_1(self, distance_coefficient, direction_coefficient):
+        self.expected_speed_A = self.expected_speed_D = self.maxspeed * distance_coefficient
+        self.expected_speed_B = self.expected_speed_C = self.maxspeed * distance_coefficient * direction_coefficient
+
+    def velocity_function_2(self, distance_coefficient, direction_coefficient):
+        self.expected_speed_B =self.expected_speed_C = self.maxspeed * distance_coefficient
+        self.expected_speed_A = self.expected_speed_D = self.maxspeed * distance_coefficient * direction_coefficient
+
+    def velocity_function_other(self, distance_coefficient, direction_coefficient):
+        pass
+
+    def speed_culculation(self, expected_pixel, length):
+        deceleration_distance = 10
+        corresponding_angle = 0.2
+        straight_range = 3
+        distance_coefficient = (length >= deceleration_distance and 1 or (length / deceleration_distance) )
+        direction_coefficient = 1 - abs(expected_pixel) * corresponding_angle / 45
+        decision_coefficient = 0
+        if expected_pixel > straight_range:
+            decision_coefficient = 1    #右
+        elif expected_pixel < -straight_range:
+            decision_coefficient = 2    #左
+        self.velocity_function(decision_coefficient)(distance_coefficient,direction_coefficient)
+
     def get_expected_speed(self, delta_pixel, length):
-        # 估算 1 pixel 对应 0.2 度
         expected_pixel = 0
-        # Propotion
+        #P
         expected_pixel += self.Kp * delta_pixel
-        # Derivative
+        #D
         derivative = delta_pixel - self.last_delta_pixel
         self.last_delta_pixel = delta_pixel
         expected_pixel = self.Td * derivative
-        # Itergrative
+        #I
         self.integrator += delta_pixel
         if self.integrator < -self.inte_max:
             self.integrator = -self.inte_max
         elif self.integrator > self.inte_max:
             self.integrator = self.inte_max
         expected_pixel += self.Tt * self.integrator
+        
+        self.speed_culculation(expected_pixel, length)
+        #根据expected_pixel和length计算expected_speed
 
-        # 根据expected_pixel和length计算expected_speed
-
-        expected_speed = 0
-        return expected_speed
+        return self.expected_speed_A, self.expected_speed_B, self.expected_speed_C, self.expected_speed_D
 
 
