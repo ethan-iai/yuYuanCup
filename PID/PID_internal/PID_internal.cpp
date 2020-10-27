c1e99617ab7be6c47a9eaadf9e5851b865264ec9
Ethan <wlx@buaa.edu.cn>
2020-10-27 14:34:22 +0800

    update the internal PID block

---
+++ PID/PID_internal/PID_internal.cpp
@@ -0,0 +1,32 @@
+#include "PID.h"
+
+int get_PID_internal(double expected_speed, double cur_speed){
+    float de; 
+	int pwm_old;
+	int pwm_cur;
+    //需要读 old pwm
+	
+    //设置PID参数
+	float kp_motor = 33;
+	float ki_motor = 0.038;
+	float kd_motor = 0.04;
+
+	pwm_old = (cur_speed - min_speed) / (max_speed - min_speed) * (max_pwm - min_pwm) + min_pwm;
+
+	for(int i = 0; i < 3; i++) {
+		error[i] = error[i + 1];
+	}
+	error[3] = expected_speed - cur_speed;	
+
+	de = kp_motor * (error[3] - error[2]) + ki_motor * error[3] -kd_motor * (error[3] - 2 * error[2] + error[1]);
+	pwm_cur = pwm_old + de;
+  	if (pwm_cur > max_pwm) {
+		pwm_cur = max_pwm;
+	} else if (pwm_cur < min_pwm) {
+		pwm_cur = min_pwm;
+	}
+	
+  	cur_speed = expected_speed;
+  	pwm_old = pwm_cur; 
+  	return pwm_cur; //输出PWM波
+}
