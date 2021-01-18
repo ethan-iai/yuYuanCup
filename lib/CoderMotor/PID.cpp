fd0420332a8647238a627ea5f95b195ca1fa6d3f
Ethan wood <wlx@buaa.edu.cn>
2021-01-18 16:02:19 +0800

    add velocity setters

--- lib/CoderMotor/PID.cpp
+++ lib/CoderMotor/PID.cpp
@@ -80,13 +80,13 @@ void set_stop_velocity() {
 }
 
 void set_pan_right_velocity() {
- speed_on_wheels[0] = speed_on_wheels[2] = MAX_SPEED;
- speed_on_wheels[1] = speed_on_wheels[3] = -MAX_SPEED;
+    speed_on_wheels[0] = speed_on_wheels[2] = MAX_SPEED;
+    speed_on_wheels[1] = speed_on_wheels[3] = -MAX_SPEED;
 }
 
 void set_pan_right_velocity() {
- speed_on_wheels[0] = speed_on_wheels[2] = -MAX_SPEED;
- speed_on_wheels[1] = speed_on_wheels[3] = MAX_SPEED;
+    speed_on_wheels[0] = speed_on_wheels[2] = -MAX_SPEED;
+    speed_on_wheels[1] = speed_on_wheels[3] = MAX_SPEED;
 }
 
 // TODO: add velocity setter of state COLLECTING
