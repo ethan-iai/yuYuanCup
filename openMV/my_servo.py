47e7c51f6bcc78d7d73e01abc42530be26dad901
Ethan Wood <wlx5502@outlook.com>
2021-03-12 08:32:55 +0800

    modify my_servo.py

--- openMV/my_servo.py
+++ openMV/my_servo.py
@@ -1,75 +1,80 @@
-import time
-from pyb import Servo
+from pyb import Servo, millis
+from math import pi, isnan
 
 class MyServo:
 
-    def __init__(self):
+    _kp = _ki = _kd = _integrator = _imax = 0
+    _last_error = _last_derivative = _last_t = 0
+    _RC = 1 / (2 * pi * 20)
+
+    def __init__(self, p=0, i=0, d=0, imax=0):
         self.pan = Servo(1)                         # P7
         self.tilt = Servo(2)                        # P8
-        self.last_delta_pixel = 0
-        self.integrator_components = [0, 0, 0]
-        self.integrator = 0
-        self.inte_max = 150
-        self.Kp = 0.5                               # proportional gain
-        self.Tt = 0.05                              # integral time constant
-        self.Td = 0.05                               # derivativa time constant
-        self.corresponding_angle = 0.2              # per pixel
-        self.maxspeed = 128
+        self._kp = float(p)
+        self._ki = float(i)
+        self._kd = float(d)
+        self._imax = abs(imax)
+        self._last_derivative = float('nan')
+        self.corresponding_angle = 0.5            # per pixel
+        self.scan_speed = 240
 
-    def init(self):
-        self.last_delta_pixel = 0
-        self.integrator_components = [0, 0, 0]
-        self.pan.angle(0)
-        self.tilt.angle(0)
+    def init(self, mode=1, tilt_angle=0):
+        if mode == 1:
+            self.pan.angle(0)
+        elif mode == 2:
+            self.pan.angle(130)
+        self.tilt.angle(tilt_angle)
 
-    def get_integrator_value(self):
-        self.integrator = 0
-        for x in range(3):
-            self.integrator += self.integrator_components[x]
-        if self.integrator > self.inte_max:
-            self.integrator = self.inte_max
-        elif self.integrator < -self.inte_max:
-            self.integrator = -self.inte_max
+    def get_pid(self, error, scaler):
+        tnow = millis()
+        dt = tnow - self._last_t
+        output = 0
+        if self._last_t == 0 or dt > 1000:
+            dt = 0
+            self.pid_clear()
+        self._last_t = tnow
+        delta_time = float(dt) / float(1000)
+        output += error * self._kp
+        if abs(self._kd) > 0 and dt > 0:
+            if isnan(self._last_derivative):
+                derivative = 0
+                self._last_derivative = 0
+            else:
+                derivative = (error - self._last_error) / delta_time
+            derivative = self._last_derivative + ((delta_time / (self._RC + delta_time)) * (derivative - self._last_derivative))
+            self._last_error = error
+            self._last_derivative = derivative
+            output += self._kd * derivative
+        output *= scaler
+        if abs(self._ki) > 0 and dt > 0:
+            self._integrator += (error * self._ki) * scaler * delta_time
+            if self._integrator < -self._imax:
+                self._integrator = -self._imax
+            elif self._integrator > self._imax:
+                self._integrator = self._imax
+            output += self._integrator
+        return output
 
-    def scan(self, count):
-        if count % 2 == 0:
-            self.pan.speed(self.maxspeed)
-        elif count % 2 == 1:
-            self.pan.speed(-self.maxspeed)
-        return self.pan.speed()
+    def pid_clear(self):
+        self._integrator = 0
+        self._last_derivative = float('nan')
 
     def rotate_steering_gear(self, delta_pixel):
-        expected_pixel = 0
-        self.integrator_components.insert(0, delta_pixel)
-        self.integrator_components.pop()
+        delta_pixel = -delta_pixel
+        pan_output = self.get_pid(delta_pixel, 1) / 2
+        self.pan.angle(self.pan.angle() + pan_output)
+        return self.pan.angle()
 
-        #P
-        expected_pixel += self.Kp * delta_pixel
-
-        #D
-        derivative = delta_pixel - self.last_delta_pixel
-        self.last_delta_pixel = delta_pixel
-        expected_pixel += self.Td * derivative
-
-        #I
-        self.get_integrator_value()
-        if self.integrator < -self.inte_max:
-            self.integrator = -self.inte_max
-        elif self.integrator > self.inte_max:
-            self.integrator = self.inte_max
-        expected_pixel += self.Tt * self.integrator
-
-        self.last_delta_pixel = delta_pixel
-
-        expected_angle = self.pan.angle() + expected_pixel * self.corresponding_angle
-        if expected_angle < -90:
-            expected_angle = -90
-        elif expected_angle > 90:
-            expected_angle = 90
-
-        self.pan.angle(expected_angle)
-
-
-
-
-
+    def scan(self, count):
+        parity = 0
+        if count % 2 == 0:
+            if parity == 1:
+                self.pid_clear()
+            parity = 0
+            self.rotate_steering_gear(self.scan_speed)
+        elif count % 2 == 1:
+            if parity == 0:
+                self.pid_clear()
+            parity = 1
+            self.rotate_steering_gear(-self.scan_speed)
+        return self.pan.angle()
